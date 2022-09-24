0b2cd591ec01a603070f5c91203a099078a9f49f
HarshBalwanix <balwaniharsh1001@gmail.com>
2022-09-24 18:02:28 +0530

    First Commit

---
+++ app/src/main/java/com/example/homesecurity/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.homesecurity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("tag", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                       // String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d("TAG", token);
+                        System.out.println("Welcome");
+                        Toast.makeText(MainActivity.this, "Welcome to Home Security by Team 77 ", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+    }
+}
